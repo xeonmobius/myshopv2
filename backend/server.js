67b53e3b92987b8c76e1f225ca1461912047d6c9
xeonmobius <refinedzeta@gmail.com>
2023-08-07 21:47:14 -0400

    feature: connected mongodb to the app with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
 
 dotenv.config();
 
+connectDB();
 const port = process.env.PORT || 5000;
 const app = express();
 
