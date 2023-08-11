2d3594d38e0074be9b7258fd421cb3009cfd8aba
xeonmobius <refinedzeta@gmail.com>
2023-08-11 17:07:18 -0400

    - added redux to query the backend instead of axios - added in some error handling for failed api calls

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -5,10 +5,9 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let statusCode = res.statusCode === 200 ? res.statusCode : 500;
   let message = err.message;
 
-  // Check for Mongoose bad ObjectId
   if (err.name === "CastError" && err.kind === "ObjectId") {
     message = "Resource not found";
     statusCode = 404;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import App from "./App";
@@ -26,7 +27,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
