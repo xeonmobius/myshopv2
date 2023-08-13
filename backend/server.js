c3ec93d7f528f1e0c43f21d983859bc15d8f1dae
xeonmobius <refinedzeta@gmail.com>
2023-08-13 12:06:17 -0400

    backend auth done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -11,11 +13,19 @@ connectDB();
 const port = process.env.PORT || 5000;
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+    const token = jwt.sign({
+        userId
+    }, process.env.JWT_SECRET, {
+        expiresIn: "30d"
+    })
+
+    // Set JWT as HTTP Only Cookie
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 1000 * 60 * 60 * 24 * 30 // 30 days
+    })
+}
+
+export default generateToken
