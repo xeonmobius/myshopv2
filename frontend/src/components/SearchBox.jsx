4189e002ba3436850712936e6c8e44eaa7a352b8
xeonmobius <refinedzeta@gmail.com>
2023-08-25 16:03:39 -0400

    fix: useParam was not imported in searchbox.jsx

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const SearchBox = () => {
   const navigate = useNavigate();
