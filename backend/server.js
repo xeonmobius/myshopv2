7c7715e7bd5202e2c4311fe00b499a5015f62e6e
xeonmobius <refinedzeta@gmail.com>
2023-08-25 06:58:11 -0400

    feature: Added build script and production to use prod react build files

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ import orderRoutes from "./routes/orderRoutes.js";
 import cookieParser from "cookie-parser";
 import uploadRoutes from "./routes/uploadRoutes.js";
 
-
 dotenv.config();
 
 connectDB();
@@ -24,14 +23,10 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/order", orderRoutes);
-app.use('/api/upload', uploadRoutes);
+app.use("/api/upload", uploadRoutes);
 
 // lets paypal get the client ID from backend
 app.get("/api/config/paypal", (req, res) => {
@@ -40,6 +35,18 @@ app.get("/api/config/paypal", (req, res) => {
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
